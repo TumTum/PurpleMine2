2feb9684cdee1b715f6b07f2492a55b86749dd8c
Tobias Matthaiou <developer@tobimat.eu>
2017-03-08 22:48:09 +0100

    Bugfix hiess anders

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker run -it --rm -v $(pwd):/data digitallyseamless/nodejs-bower-grunt:latest grant default
+docker run -it --rm -v $(pwd):/data digitallyseamless/nodejs-bower-grunt:latest grunt default
